8796a5e804838c6a813f45fb5d27de6e773e4636
kwonsangmin <rnjstkdals01@gmail.com>
2023-11-08 14:17:25 +0900

    snangmin commit test 1

--- app/src/main/java/com/example/wonjokwon/MainActivity.kt
+++ app/src/main/java/com/example/wonjokwon/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         print("sibal");
-        print("commit test sangmin")
+        print("commit test sangmin branch checout")
     }
 }
