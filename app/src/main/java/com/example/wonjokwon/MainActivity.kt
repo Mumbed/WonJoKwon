f7e42ed184770aaf3abc2574ac5d60dfdadfb1df
myeongjincho <chomj12233@gmail.com>
2023-11-08 15:29:46 +0900

    myeongjin commit test1

--- app/src/main/java/com/example/wonjokwon/MainActivity.kt
+++ app/src/main/java/com/example/wonjokwon/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.wonjokwon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gomore.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         print("sibal");
         print("commit test sangmin")
+        print("commit myeongjin")
     }
 }
