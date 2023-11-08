c21223aadbb10ccb97f1334c64f18b0dde090d3b
myeongjincho <chomj12233@gmail.com>
2023-11-08 15:22:25 +0900

    myeongjin commit1

--- app/src/main/java/com/example/wonjokwon/MainActivity.kt
+++ app/src/main/java/com/example/wonjokwon/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.wonjokwon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gomore.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
