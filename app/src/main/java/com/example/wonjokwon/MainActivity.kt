0ebe5678d94240cb75e47e43a4facd4d10aff69e
ys <dudtlsdl00@naver.com>
2023-11-12 23:45:23 +0900

    공

--- app/src/main/java/com/example/wonjokwon/MainActivity.kt
+++ app/src/main/java/com/example/wonjokwon/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("sibal");
+        print("파베 연결 성공");
     }
 }
